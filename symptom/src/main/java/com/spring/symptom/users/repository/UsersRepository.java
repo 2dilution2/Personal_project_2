65efff8e122f661b1503dc2ca9e1a736667e51f2
2dilution2 <2dilution2@gmail.com>
2023-08-30 22:43:53 +0900

    스프링 부트 버전 다운그레이드 및 세팅 변경

--- symptom/src/main/java/com/spring/symptom/users/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.symptom.users.repository;
-
-import com.spring.symptom.users.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    Users findByEmail(String email);
-    boolean existsByEmail(String email);
-}

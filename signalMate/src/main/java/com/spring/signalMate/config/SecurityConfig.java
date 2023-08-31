2a2325683173ee2bf3aec0341b73b1a4f6ab50d0
2dilution2 <2dilution2@gmail.com>
2023-08-31 17:50:11 +0900

    SpringSecurity 기본 폼 설정

--- signalMate/src/main/java/com/spring/signalMate/config/SecurityConfig.java
+++ signalMate/src/main/java/com/spring/signalMate/config/SecurityConfig.java
@@ -1,51 +1,34 @@
 package com.spring.signalMate.config;
 
-import com.spring.signalMate.security.JwtConfigurer;
-import com.spring.signalMate.security.JwtTokenProvider;
-import com.spring.signalMate.users.repository.UsersRepository;
-import com.spring.signalMate.users.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    private final UsersRepository usersRepository;
-
-    @Autowired
-    private JwtTokenProvider tokenProvider;
-
-    public SecurityConfig(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService(usersRepository);
-    }
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .apply(jwt());
-    }
-
-    public JwtConfigurer jwt() {
-        return new JwtConfigurer(tokenProvider);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authz -> authz.anyRequest().authenticated())
+                .formLogin(withDefaults());
+        return http.build();
     }
 }
--- signalMate/src/main/java/com/spring/signalMate/users/controller/UsersController.java
+++ signalMate/src/main/java/com/spring/signalMate/users/controller/UsersController.java
@@ -1,19 +1,9 @@
 package com.spring.signalMate.users.controller;
 
-import com.spring.signalMate.security.JwtTokenProvider;
-import com.spring.signalMate.security.payload.JwtAuthenticationResponse;
-import com.spring.signalMate.security.payload.LoginRequest;
 import com.spring.signalMate.users.dto.UsersDto;
 import com.spring.signalMate.users.entity.Users;
 import com.spring.signalMate.users.service.UsersService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,8 +12,6 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
 
     private final UsersService usersService;
-    private final JwtTokenProvider tokenProvider;
-    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public Users register(@RequestBody UsersDto usersDto) {
@@ -40,19 +28,4 @@ public class UsersController {
         usersService.delete(userId);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Validated @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getEmail(),
-                        loginRequest.getPassword()
-                )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = tokenProvider.generateToken(authentication);
-        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
-    }
 }

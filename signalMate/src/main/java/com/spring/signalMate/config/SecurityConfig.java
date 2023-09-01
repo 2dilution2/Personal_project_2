1d3072900438b62323b41be9ab493aeab65dd3e5
2dilution2 <2dilution2@gmail.com>
2023-09-01 13:25:07 +0900

    임시 페이지 설정

--- signalMate/src/main/java/com/spring/signalMate/config/SecurityConfig.java
+++ signalMate/src/main/java/com/spring/signalMate/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -26,8 +25,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authz -> authz.anyRequest().authenticated())
-                .formLogin(login -> login.loginPage("/login").permitAll());
+        http.authorizeRequests(authz -> authz
+                        .requestMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(login -> login.loginPage("/custom-login").permitAll());
         return http.build();
     }
+
 }
